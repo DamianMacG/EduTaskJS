81dab9cdf3fd9a1008f6229f84a002a21d3c086f
damiang <damiangildea12@gmail.com>
2024-01-17 09:20:39 +0000

    Added test-data teachers list

---
+++ back-end/db/test-data/teachers.js
@@ -0,0 +1,14 @@
+module.exports = [
+    { name: 'Teacher Smith', email: 'smith@example.com', role: 'teacher', password: 'testteacherpass' },
+    { name: 'Teacher Turner', email: 'turner@example.com', role: 'teacher', password: 'testteacher123' },
+    { name: 'Teacher Robinson', email: 'robinson@example.com', role: 'teacher', password: 'testteacher456' },
+    { name: 'Teacher White', email: 'white@example.com', role: 'teacher', password: 'testteacher789' },
+    { name: 'Teacher Brown', email: 'brown@example.com', role: 'teacher', password: 'testteacherabc' },
+    { name: 'Teacher Garcia', email: 'garcia@example.com', role: 'teacher', password: 'testteacherdef' },
+    { name: 'Teacher Davis', email: 'davis@example.com', role: 'teacher', password: 'testteacherghi' },
+    { name: 'Teacher Johnson', email: 'johnson@example.com', role: 'teacher', password: 'testteacherjkl' },
+    { name: 'Teacher Miller', email: 'miller@example.com', role: 'teacher', password: 'testteachermno' },
+    { name: 'Teacher Lee', email: 'lee@example.com', role: 'teacher', password: 'testteacherpqr' },
+    { name: 'Teacher Turner', email: 'turner@example.com', role: 'teacher', password: 'testteachertstu' },
+    { name: 'Teacher Robinson', email: 'robinson@example.com', role: 'teacher', password: 'testteachervwx' },
+];

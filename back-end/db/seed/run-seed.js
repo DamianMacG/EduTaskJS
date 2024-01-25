da4ca467194a5b20e87d0a0382f82e9c4f06a615
damiang <damiangildea12@gmail.com>
2024-01-25 13:39:48 +0000

    run-seed fix

--- back-end/db/seed/run-seed.js
+++ back-end/db/seed/run-seed.js
@@ -1,12 +1,11 @@
 const seed = require("./seed");
 const devData = require("../dev-data/index");
 const pool = require("../connection");
-const testData = require('../test-data/index')
 
 const runSeed = async () => {
   try {
     console.log("Seeding...");
-    await seed(testData);
+    await seed(devData);
     console.log("Data seeded successfully");
   } catch (error) {
     console.error("Error seeding data:", error);

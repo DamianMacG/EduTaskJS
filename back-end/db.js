9a832233ddb30488c928d5bcea1555f9068a282c
damiang <damiangildea12@gmail.com>
2024-01-25 10:49:01 +0000

    Added index file to test-data

--- back-end/db.js
+++ back-end/db.js
@@ -1,11 +1,11 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "edutask2",
-  password: "Simpson1",
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "edutask2",
+//   password: "Simpson1",
+//   port: 5432,
+// });
 
-module.exports = pool;
+// module.exports = pool;

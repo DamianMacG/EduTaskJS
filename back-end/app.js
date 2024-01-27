f730c52290e7d0e141f49b1bf052a809e5e6545a
damiang <damiangildea12@gmail.com>
2024-01-27 10:32:04 +0000

    Adding routes to app.js and testing

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const studentRoutes = require("./routers/studentRouter");
+const teacherRoutes = require("./routers/teacherRouter");
+const assignmentsRoutes = require("./routers/assignmentsRouter");
 
 const {
   handleCustomErrors,
@@ -12,6 +14,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/students", studentRoutes);
+app.use("/api/v1/teachers", teacherRoutes);
+app.use("/api/v1/assignments", assignmentsRoutes);
 
 app.all("*", (_, res) => {
   res.status(404).send({ msg: "Not found" });
